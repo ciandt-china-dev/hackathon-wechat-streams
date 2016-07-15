784a6fb26ba3b259d746717e6b12d96d724a6408
Sean <seans@ciandt.com>
2016-07-15 19:30:21 +0800

    Add tables image tag image_tag and enable eloquent orm

---
+++ stream-server/app/Models/Tag.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model{
+
+  public function images()
+  {
+    return $this->belongsToMany('App\Models\Image');
+  }
+}
