6ba7f639d4db7370ded503c7e4c564d34a5ebe5f
Xiadan Wang <bala@miyapps.com>
2016-07-15 19:26:26 +0800

    reaction to respond to user's input

--- stream-server/app/Http/routes.php
+++ stream-server/app/Http/routes.php
@@ -119,53 +119,55 @@ $app->post('/api/streams', function(Illuminate\Http\Request $request) use ($app)
     $sReqTimeStamp = $request->input('timestamp');
     $sReqNonce = $request->input('nonce');
 
+    // Obtain the raw data from wechat server
     $sReqData = file_get_contents('php://input');
-
 
     $sMsg = ""; // decrypted plain text
     $errCode = $wxcpt->DecryptMsg($sReqMsgSig, $sReqTimeStamp, $sReqNonce, $sReqData, $sMsg);
 
-    // return var_export($sMsg,1);
-
     if ($errCode == 0) {
-        // 解密成功，sMsg即为xml格式的明文
-        // TODO: 对明文的处理
-        // For example:
         $xml = new \DOMDocument();
-
         $xml->loadXML($sMsg);
 
-        $msgType = $xml->getElementsByTagName('MsgType')[0]->nodeValue;
-        $agentId = $xml->getElementsByTagName('AgentID')[0]->nodeValue;
-        $msgId =  $xml->getElementsByTagName('MsgId')[0]->nodeValue;
-        $createTime = datetime();
-        $fromUserName = $xml->getElementsByTagName('ToUserName')[0]->nodeValue;
-        $toUserName = $xml->getElementsByTagName('FromUserName')[0]->nodeValue;
+        $msgType = $xml->getElementsByTagName('MsgType')->item(0)->nodeValue;
+        $fromUserName = $xml->getElementsByTagName('ToUserName')->item(0)->nodeValue;
+        $toUserName = $xml->getElementsByTagName('FromUserName')->item(0)->nodeValue;
 
         if ($msgType == 'image')
         {
-            // Save the image and response the image Id and the format "%ImgId #tag1 #tag2 #tag3 Comment"
-            $sRespData = "<xml><ToUserName><![CDATA[$toUserName]]></ToUserName><FromUserName><![CDATA[$fromUserName]]></FromUserName><CreateTime>$createTime</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%ImgId #tag1 #tag2 #tag3 Comment]]></Content><MsgId>$msgId</MsgId><AgentID>$agentId</AgentID></xml>";
+            $sReqData = "<xml>
+   <ToUserName><![CDATA[$toUserName]]></ToUserName>
+   <FromUserName><![CDATA[$fromUserName]]></FromUserName> 
+   <CreateTime>$sReqTimeStamp</CreateTime>
+   <MsgType><![CDATA[text]]></MsgType>
+   <Content><![CDATA[%ImgId #tag1 #tag2 #tag3 Comment]]></Content>
+</xml>";
         }
         elseif ($msgType == 'text')
         {
+            // Todo: the message should display depends on when the user's input is valid
+            $message = "You have successfully tagged your photo";
+
             // Parse the text %ImgId #tag1 #tag2 #tag3 Comment
-            $sRespData = "<xml><ToUserName><![CDATA[$toUserName]]></ToUserName><FromUserName><![CDATA[$fromUserName]]></FromUserName><CreateTime>$createTime</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[You have tagged the image]]></Content><MsgId>$msgId</MsgId><AgentID>$agentId</AgentID></xml>";
+            $sReqData = "<xml>
+   <ToUserName><![CDATA[$toUserName]]></ToUserName>
+   <FromUserName><![CDATA[$fromUserName]]></FromUserName> 
+   <CreateTime>$sReqTimeStamp</CreateTime>
+   <MsgType><![CDATA[text]]></MsgType>
+   <Content><![CDATA[$message]]></Content>
+</xml>";
         }
 
         // Encrypt the message and response to user
         $sEncryptMsg = "";
-        $errCode = $wxcpt->EncryptMsg($sRespData, $sReqTimeStamp, $sReqNonce, $sEncryptMsg);
+        $errCode = $wxcpt->EncryptMsg($sReqData, $sReqTimeStamp, $sReqNonce, $sEncryptMsg);
+
         if ($errCode == 0) {
-            // TODO:
-            // 加密成功，企业需要将加密之后的sEncryptMsg返回
-            // HttpUtils.SetResponce($sEncryptMsg);  //回复加密之后的密文
             return $sEncryptMsg;
         }
         else
         {
             return "ERR: " . $errCode;
-            // exit(-1);
         }
     }
     else
