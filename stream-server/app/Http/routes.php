ea610e3cad143528227f4f2408b0241a9c34dbec
Xiadan Wang <bala@miyapps.com>
2016-07-15 14:10:36 +0800

    add a get /streams route

--- stream-server/app/Http/routes.php
+++ stream-server/app/Http/routes.php
@@ -14,3 +14,36 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+
+/**
+ * Verify wechat
+ */
+$app->get('/streams', function (Illuminate\Http\Request $request) use ($app) {
+    $wxcpt = new WechatEnterprise\WXBizMsgCrypt(
+        env('WECHATENTERPRISE_TOKEN'),
+        env('WECHATENTERPRISE_AESKEY'),
+        env('WECHATENTERPRISE_CORPID'));
+
+    $sVerifyMsgSig = $request->input('msg_signature');
+    $sVerifyTimeStamp = $request->input('timestamp');
+    $sVerifyNonce = $request->input('nonce');
+    $sVerifyEchoStr = $request->input('echostr');
+    $errCode = $wxcpt->VerifyURL($sVerifyMsgSig, $sVerifyTimeStamp, $sVerifyNonce, $sVerifyEchoStr, $sEchoStr);
+
+    if ($errCode == 0) {
+        //
+        // 验证URL成功，将sEchoStr返回
+        // HttpUtils.SetResponce($sEchoStr);
+        return $sEchoStr;
+    } else {
+        return "ERR: " . $errCode;
+    }
+});
+
+/**
+ * Handler wechat response and trigger proper methods
+ */
+$app->post('/streams', function() use ($app) {
+
+});
